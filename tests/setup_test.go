07a51b3cdd2b80600c0d7d709820947dbc8870a0
Eko <ekolesmanasijabat@gmail.com>
2023-03-05 08:50:35 +0800

    eight

--- tests/setup_test.go
+++ tests/setup_test.go
@@ -18,7 +18,8 @@ var postInstance = models.Post{}
 
 func TestMain(m *testing.M) {
 	var err error
-	err = godotenv.Load(os.ExpandEnv("./../.env"))
+	//err = godotenv.Load(os.ExpandEnv("./../.env"))
+	err = godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Error getting env %v\n", err)
 	}
